60f1db7472f099cb19a3fd3c8357cca92d949ca9
Nina Nguyen <ninamailan90@gmail.com>
2020-03-27 12:03:15 -0700

    update main.tf

--- terraform-sample/main.tf
+++ terraform-sample/main.tf
@@ -4,3 +4,22 @@ terraform {
 }
 
 #Provider
+provider "aws" {
+    version = "~> 2.0"
+    region = "us-east-1"
+    shared_credentials_file = "~/.aws/creds"
+}
+
+#Resource
+## Note: 'bucekt1' is a REFERENCE LABEL. Not actual name in S3.
+## To do so, add an ATTRIBUTE
+resource "aws_s3_bucket" "bucket1" {
+    bucket = "bucket1"
+}
+
+#Data Sources
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
