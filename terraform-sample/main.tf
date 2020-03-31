5bfe578321cf560be08418789fb37fd7788d5a85
Nina Nguyen <ninamailan90@gmail.com>
2020-03-31 12:10:13 -0700

    update terraform code

--- terraform-sample/main.tf
+++ terraform-sample/main.tf
@@ -11,10 +11,23 @@ provider "aws" {
 }
 
 #Resource
-## Note: 'bucekt1' is a REFERENCE LABEL. Not actual name in S3.
-## To do so, add an ATTRIBUTE
+## Note: 'bucket1' is a REFERENCE LABEL. Not actual name in S3.
+## To do so, add an ATTRIBUTE (or use outputs)
 resource "aws_s3_bucket" "bucket1" {
     bucket = "bucket1"
+}
+
+output "bucket_info" {
+    value = aws_s3_bucket.bucket1
+}
+
+## Note: need to use "data." prefix to access data
+output  "aws_caller_info" {
+    value = data.aws_caller_identity.current
+}
+
+output "aws_availability_info" {
+    value = data.aws_availability_zones.available
 }
 
 #Data Sources
